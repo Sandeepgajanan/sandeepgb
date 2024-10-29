89b1a65f8bf2c63500eb744fd2488f23d0fa846f
Sandeepgajanan <beingsandeepg@gmail.com>
2024-10-29 17:36:17 +0530

    added respo

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -4,7 +4,7 @@ import Heading from "./Heading";
 
 const Projects = () => {
   return (
-    <section className="w-full h-screen flex flex-col py-5 max-sm:py-3 ">
+    <section className="w-full h-screen flex flex-col py-5 max-sm:py-3 max-sm:h-fit ">
       <Heading text={"Featured Projects"} />
       <p className="text-gray-400 mb-4  text-center">← Scroll to see more →</p>
       <div className="mt-10 flex flex-nowrap gap-8 overflow-x-auto w-full px-5 max-sm:snap-x max-sm:snap-mandatory max-sm:mt-2" id="projects">
